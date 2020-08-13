ca0f0f14adb0b92666e876be2207d6e86fa25555
Alexandr <valverin95@gmail.com>
2020-08-13 20:57:06 +0300

    init project

---
+++ src/app/template/navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function Navbar() {
+  return (
+    <nav className="navbar mb-6">
+      <div className="container">
+        <div className="navbar-brand">
+          <div className="navbar-item">
+            <img src="https://bulma.io/images/bulma-logo.png" alt="" width="112" height="28"/>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
